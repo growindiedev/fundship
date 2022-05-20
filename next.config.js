3f218d37fa50c6f22d2790583d4a5f80b6ca7640
Abhishek Ranjan <rabhishekranjan@gmail.com>
2022-05-20 08:19:13 +0530

    created hardhat.config, deploy.js localhost

--- next.config.js
+++ next.config.js
@@ -1,25 +1,6 @@
-/* hardhat.config.js */
-require("@nomiclabs/hardhat-waffle");
-
-module.exports = {
-  defaultNetwork: "hardhat",
-  networks: {
-    hardhat: {
-      chainId: 1337,
-    },
-    //  unused configuration commented out for now
-    //  mumbai: {
-    //    url: "https://rpc-mumbai.maticvigil.com",
-    //    accounts: [process.env.privateKey]
-    //  }
-  },
-  solidity: {
-    version: "0.8.4",
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200,
-      },
-    },
-  },
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
 };
+
+module.exports = nextConfig;
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,1 +1,25 @@
+const hre = require("hardhat");
+const fs = require("fs");
 
+async function main() {
+  const Crowdfunding = await hre.ethers.getContractFactory("Crowdfunding");
+  const crowdfunding = await Crowdfunding.deploy();
+
+  await crowdfunding.deployed();
+
+  console.log("Crowdfunding deployed to:", crowdfunding.address);
+
+  fs.writeFileSync(
+    "./config.js",
+    `
+  export const crowdfundingAddress = "${crowdfunding.address}"
+  `
+  );
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
